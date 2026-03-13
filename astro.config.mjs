090551479246238150e4f9db1b6e4047e5a6d797
sawachier <sawachier@users.noreply.github.com>
2026-03-13 22:25:16 +0100

    fix: ajouter slash final au base URL pour corriger les liens

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://sawachier.github.io',
-  base: '/soireeGuitare',
+  base: '/soireeGuitare/',
   integrations: [sitemap()],
 });
